1f8daf846ba856e9d48f7721a5924b854102bed4
Luis Barretto <165199422+LuisBarretto11@users.noreply.github.com>
2024-11-04 20:56:04 -0300

    Update listagem.controller.php

--- projeto-php/controllers/listagem.controller.php
+++ projeto-php/controllers/listagem.controller.php
@@ -1,3 +1,33 @@
-<?php 
-require ('../models/listagem.view.php');
+<?php
 require ('../models/listagem.models.php');
+require ('../models/listagem.view.php');
+
+
+class ListagemController {
+    public function adicionarVoo() {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_GET['action'] == 'adicionar') {
+            $numero_voo = $_POST['numero_voo'];
+            $origem = $_POST['origem'];
+            $destino = $_POST['destino'];
+            $data = $_POST['data'];
+            $horario = $_POST['horario'];
+            $status = $_POST['status'];
+        
+            // Verifique se os dados foram preenchidos
+            if (!empty($numero_voo) && !empty($origem) && !empty($destino) && !empty($data) && !empty($horario) && !empty($status)) {
+                $voosModel = new VoosModel();
+                $voosModel->adicionarVoo($numero_voo, $origem, $destino, $data, $horario, $status);
+        
+                // Mensagem de sucesso e redirecionamento
+                $_SESSION['mensagem'] = "Voo $numero_voo adicionado com sucesso!";
+                header('Location: ../controllers/ListagemController.php?action=listar');
+                exit;
+            } else {
+                // Exibir mensagem de erro se algum campo estiver vazio
+                $_SESSION['mensagem'] = "Todos os campos são obrigatórios.";
+                header('Location: ../controllers/ListagemController.php?action=adicionar');
+                exit;
+            }
+        }
+    }
+    }

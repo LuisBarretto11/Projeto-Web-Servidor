d72fffa4a74f1345491e7ec58a958997fbc4143d
Luiz Cardoso <168668714+Luiz-Cardoso@users.noreply.github.com>
2024-12-02 22:19:19 -0300

    Update listagem.php

--- projeto-php/listagem.php
+++ projeto-php/listagem.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listagem</title>
-    <link rel="stylesheet" href="css/listagem.css">
-</head>
-<body>
-    <div class="container">
-        <div class="home-buttons">
-            <?php include ('views/listagem.view.php'); ?>
-        </div>
-    </div>
-</body>
-</html>
+<?php
+require_once 'controllers/voo.controller.php';
+
+$vooController =  new VooController();
+$vooController->listar();
+
+require_once 'views/voo.listar.view.php';

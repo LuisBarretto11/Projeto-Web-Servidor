b65bbe7ca83736ae7634df65fbe9d0db2c38f6ff
Luiz Cardoso <168668714+Luiz-Cardoso@users.noreply.github.com>
2024-12-02 22:28:31 -0300

    Update voo.listar.view.php

---
+++ projeto-php/views/voo.listar.view.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem de Voos</title>
+    <link rel="stylesheet" href="css/listagem.css"> 
+</head>
+<body>
+    <div class="container-voos">
+        <h2>Lista de Voos</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Destino</th>
+                    <th>Data</th>
+                    <th>Hora</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($voos as $voo):  ?>
+                    <tr>
+                        <td> <?php echo $voo->id; ?></td>
+                        <td> <?php echo $voo->destino; ?></td>
+                        <td> <?php echo $voo->data_voo; ?></td>
+                        <td> <?php echo $voo->horario; ?></td>
+                        <td> <?php echo $voo->status; ?></td>
+                        <td>
+                            <a href="voo.controller.php?action=remover&id= <?php echo $voo->id;?> ">Remover</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <a href="cadastro.php" class="btn-add">Adicionar Voo</a>
+        <a href="javascript:history.back()" class="btn-voltar">Voltar</a>
+    </div>
+</body>
+</html>
+
+
+
+
+
+

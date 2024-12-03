b094277f2d5ebf20cc79e4b97b2cf4b3c88debd3
Luiz Cardoso <168668714+Luiz-Cardoso@users.noreply.github.com>
2024-12-02 23:30:11 -0300

    Update listagem.php

--- projeto-php/listagem.php
+++ projeto-php/listagem.php
@@ -2,6 +2,18 @@
 require_once 'controllers/voo.controller.php';
 
 $vooController =  new VooController();
-$vooController->listar();
+$action = $_GET['action'] ?? 'listar';
+
+switch($action){
+    case 'listar':
+        $vooController->listar();
+        break;
+    case 'excluir':
+        $id = $_GET['id'] ?? null;
+        if($id){
+            $vooController->excluir($id);
+            break;
+        }
+}
 
 require_once 'views/voo.listar.view.php';

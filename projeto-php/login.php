4ec3aa9240d2c578cd7125adb1e4b5a2d1cbf426
Luiz Cardoso <168668714+Luiz-Cardoso@users.noreply.github.com>
2024-12-02 22:18:29 -0300

    Update login.php

--- projeto-php/login.php
+++ projeto-php/login.php
@@ -1,16 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="css/login.css">
-</head>
-<body>
-    <div class="container">
-        <?php include ('views/login.view.php'); ?>        
-    </div>
-</body>
-</html>
+<?php
+require_once 'controllers/usuario.controller.php';
 
 
+session_start();
+
+if(isset($_SESSION['login_error'])){
+    $login_error = $_SESSION['login_error'];
+    unset($_SESSION['login_error']);
+}
+
+
+
+$action = $_GET['action'] ?? 'login';
+
+switch($action){
+    case 'login':
+        require_once 'views/usuario.login.view.php';
+        break;
+    case 'autenticar':
+        $controller = new UsuarioController();
+        $controller->login();
+        break;
+    default:
+        require_once 'views/usuario.login.view.php';
+        break;
+}
